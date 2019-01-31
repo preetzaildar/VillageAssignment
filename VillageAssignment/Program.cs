d18d0e28ac6cfbbb23a8b729733ea0049e142638
visualstudio <visualstudio@230-PC30>
2019-01-31 15:04:35 -0500

    731788

---
+++ VillageAssignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VillageAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
